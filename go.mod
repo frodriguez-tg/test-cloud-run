0363ea2d9bc21ad55a7e1d95be1e0129d6479609
Fredy Rodriguez <fredyr@trugolf.com>
2023-07-14 13:53:37 -0600

    Add connection to DB
    Signed-off-by: Fredy Rodriguez <fredyr@trugolf.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module github.com/frodriguez-tg/test
 
 go 1.20
+
+require (
+	entgo.io/ent v0.12.3
+	github.com/jackc/pgx/v5 v5.4.2
+	github.com/jmoiron/sqlx v1.3.5
+)
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,11 +5,30 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"entgo.io/ent/dialect/sql"
+	"github.com/jmoiron/sqlx"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func main() {
 	log.Print("starting server...")
 	http.HandleFunc("/", handler)
+
+	dbURL := os.Getenv("V3_DBURL")
+
+	db, err := sqlx.Open("pgx", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Create an ent.Driver from `db`.
+	driver := sql.OpenDB("postgres", db.DB)
+	if driver.DB().Ping() != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("ping to DB successfully")
 
 	// Determine port for HTTP service.
 	port := os.Getenv("PORT")
