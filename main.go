4f784bdfd7d003bd2df1d9d6dc7e1fdcb7840ae0
Fredy Rodriguez <fredyr@trugolf.com>
2023-07-17 14:09:47 -0600

    Change to app engine
    Signed-off-by: Fredy Rodriguez <fredyr@trugolf.com>

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"hash/crc32"
 	"log"
 	"net/http"
 	"os"
 
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
 	"entgo.io/ent/dialect/sql"
-
 	"github.com/jmoiron/sqlx"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
@@ -17,8 +20,13 @@ func main() {
 	log.Print("starting server...")
 	http.HandleFunc("/", handler)
 
-	dbURI := os.Getenv("V3_DBURL")
+	secretDB := os.Getenv("SECRET")
 
+	dbURI, err := getDBURI(secretDB)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(dbURI)
 	db, err := sqlx.Open("pgx", dbURI)
 	if err != nil {
 		log.Fatalf("failed to connect to db: %v", err)
@@ -50,4 +58,33 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		name = "mate"
 	}
 	fmt.Fprintf(w, "Hello %s!\n", name)
+}
+
+func getDBURI(name string) (string, error) {
+	ctx := context.Background()
+	client, err := secretmanager.NewClient(ctx)
+	if err != nil {
+		return "", fmt.Errorf("failed to create secretmanager client: %w", err)
+	}
+	defer client.Close()
+
+	// Build the request.
+	req := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: name,
+	}
+
+	// Call the API.
+	result, err := client.AccessSecretVersion(ctx, req)
+	if err != nil {
+		return "", fmt.Errorf("failed to access secret version: %w", err)
+	}
+
+	// Verify the data checksum.
+	crc32c := crc32.MakeTable(crc32.Castagnoli)
+	checksum := int64(crc32.Checksum(result.Payload.Data, crc32c))
+	if checksum != *result.Payload.DataCrc32C {
+		return "", fmt.Errorf("Data corruption detected.")
+	}
+
+	return string(result.Payload.Data), nil
 }
